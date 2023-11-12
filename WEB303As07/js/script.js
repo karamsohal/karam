b3550026108e734425b3eb66de88ef19145dc76c
karamsohal <122296221+karamsohal@users.noreply.github.com>
2023-11-11 22:29:06 -0500

    Flies updated

--- WEB303As07/js/script.js
+++ WEB303As07/js/script.js
@@ -1,11 +1,29 @@
 $(function () {
-    $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) {
+    $('#photo-viewer').show().on('click', '.photo-box', function x(e) {
         var $content = $(this).clone().find('img').css({
             marginLeft: 0,
             marginTop: 0,
             width: '100%',
             height: 'auto'
         });
-        //modal code goes here
+    
+        var modal = $("#myModal");
+       
+ 
+        var modalImg = $("#img");
+        $('.photo-box').on('click',  function(e){
+            $(modal).show()
+            $(modalImg).attr('src', $('.active').children('.myImg').map(function () {
+                return $(this).attr('src')
+                }).get());
+        })
+        
+
+        var span = $(".close");
+    
+        span.on('click', function(e) { 
+            e.preventDefault();
+            $(modal).hide();
+        })
     });
-});
+    });
